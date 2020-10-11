7f8ea4e8f65a47bb87ef4eb2fe7e91375f2affd3
AlexTS1980 <ater1980@gmail.com>
2020-10-11 22:07:38 +0100

    plots fixed

--- config_classifier.py
+++ config_classifier.py
@@ -20,9 +20,9 @@ def get_config_pars_classifier(stage):
                              help="Either this or pretrained classifier must be defined!")
         parser_.add_argument("--save_dir", type=str, default="saved_models",
                              help="Directory to save checkpoints")
-        parser_.add_argument("--train_data_dir", type=str, default='covid_data/cncb/train',
+        parser_.add_argument("--train_data_dir", type=str, default='covid_data/cncb/download/train',
                              help="Path to the training data. Must contain images and binary masks")
-        parser_.add_argument("--val_data_dir", type=str, default='covid_data/cncb/val',
+        parser_.add_argument("--val_data_dir", type=str, default='covid_data/cncb/download/val',
                              help="Path to the validation data. Must contain images and binary masks")
         parser_.add_argument("--batch_size", type=int, default=8, help="Implemented only for batch size = 1")
         parser_.add_argument("--save_every", type=int, default=10)
@@ -32,7 +32,7 @@ def get_config_pars_classifier(stage):
         parser_.add_argument("--ckpt", type=str,
                              help="Checkpoint file in .pth format. "
                                   "Must contain the following keys: model_weights, optimizer_state, anchor_generator")
-        parser_.add_argument("--test_data_dir", type=str, default='covid_data/cncb/test',
+        parser_.add_argument("--test_data_dir", type=str, default='covid_data/cncb/download/test',
                              help="Path to the test data. Must contain images and may contain binary masks")
 
     model_args = parser_.parse_args()
